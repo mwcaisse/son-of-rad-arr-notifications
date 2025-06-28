83649f66d3e8cbc07a43f004567eaddbedfb11a6
Mitchell Caisse <mitchell@mwcaisse.com>
2025-06-28 19:12:53 -0400

    Handle both File Import and Import Completed from Sonar, they return different formats despite having the same event type

--- Sonarr/Payloads/SonarrImportPayload.cs
+++ Sonarr/Payloads/SonarrImportPayload.cs
@@ -2,13 +2,22 @@ using SonOfRadArrNotifications.Sonarr.Payloads.Models;
 
 namespace SonOfRadArrNotifications.Sonarr.Payloads;
 
+
 public class SonarrImportPayload : SonarrPayloadBase
 {
     public SonarrSeries Series { get; set; }
     
     public List<SonarrEpisode> Episodes { get; set; }
     
+    /// <summary>
+    /// Sonarr has two events that use the "Download" event type, File Import and Import Complete
+    ///
+    /// One of them will have a list of EpisodeFiles while one will have a singular Episode File.
+    ///     We will add both of them so that we can handle both payload types
+    /// </summary>
     public List<SonarrEpisodeFile> EpisodeFiles { get; set; }
+    
+    public SonarrEpisodeFile? EpisodeFile { get; set; }
     
     public string DownloadClient { get; set; }
     
@@ -23,5 +32,19 @@ public class SonarrImportPayload : SonarrPayloadBase
     public string SourcePath {get; set;}
     
     public string DestinationPath {get; set;}
+
+    public SonarrEpisodeFile? GetFirstEpisodeFile()
+    {
+        if (EpisodeFile != null)
+        {
+            return EpisodeFile;
+        }
+        if (EpisodeFiles.Count > 0)
+        {
+            return EpisodeFiles[0];
+        }
+
+        return null;
+    }
     
 }
